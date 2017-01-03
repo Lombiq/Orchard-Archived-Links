a0e405a899b62f328884a9735722911516c14cb7
Szabados Ádám <adam.szabados@lombiq.com>
2017-01-03 16:26:20 +0100

    Adding ArchivedLinkRouteProvider file
    --HG--
    branch : dev

---
+++ Routes/ArchivedLinkRouteProvider.cs
@@ -0,0 +1,47 @@
+﻿using Orchard.Mvc.Routes;
+using System.Collections.Generic;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Lombiq.ArchivedLinks.Routes
+{
+    public class ArchivedLinkRouteProvider : IRouteProvider
+    {
+        public void GetRoutes(ICollection<RouteDescriptor> routes)
+        {
+            foreach (var routeDescriptor in GetRoutes())
+            {
+                routes.Add(routeDescriptor);
+            }
+        }
+
+        public IEnumerable<RouteDescriptor> GetRoutes()
+        {
+            const string Area = "Lombiq.ArchivedLinks";
+            const string Controller = "ArchivedLink";
+
+            return new[]
+            {
+                new RouteDescriptor
+                {
+                    Priority = 5,
+                    Route = new Route(
+                        "jumpifworking/{originalUrl}",
+                        new RouteValueDictionary
+                        {
+                            { "area", Area },
+                            { "controller", Controller },
+                            { "action", "Index" },
+                            { "originalUrl", UrlParameter.Optional}
+                        },
+                        new RouteValueDictionary(),
+                        new RouteValueDictionary
+                        {
+                            { "area", Area }
+                        },
+                        new MvcRouteHandler())
+                },
+            };
+        }
+    }
+}
